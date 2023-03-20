6b39a73d1c8ccb0b204b70a02b1085f873b7c008
Oleksiy12 <betbonnay@gmail.com>
2023-03-20 20:43:17 +0200

    Update task1.js

--- js/task1.js
+++ js/task1.js
@@ -1,4 +1,12 @@
-const quantityLi = document.querySelectorAll(`.item`);
+const quantityLi = document.querySelectorAll(".item");
 console.log("Number of categories: ", quantityLi.length);
 
-const nameUl = 
+const listWithClass = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listWithClass.length}`);
+listWithClass.forEach(listEl => {
+  console.log('');
+  console.log(`Category: ${listEl.firstElementChild.textContent}`);
+  console.log(
+    `Elements: ${listEl.lastElementChild.querySelectorAll('li').length}`,
+  );
+});
