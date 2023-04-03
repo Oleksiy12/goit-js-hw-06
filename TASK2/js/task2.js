2409386cc4688b55e12c25c95c7273c28d524321
Oleksiy12 <betbonnay@gmail.com>
2023-04-03 20:27:36 +0300

    update3

--- TASK2/js/task2.js
+++ TASK2/js/task2.js
@@ -5,6 +5,14 @@ const ingredients = [
     "Tomatos",
     "Herbs",
     "Condiments",
-  ];
+  ]; 
 
-  console.log(ingredients);
+
+const firstValue = document.querySelector("#ingredients");
+const newList = ingredients.forEach((value) => {
+  let createElement = document.createElement("li");
+  createElement.textContent = value;
+  createElement.classList.add("item");
+  firstValue.append(createElement);
+})
+
