0bc09c28cc3d2620511dab3378437b7a82a0d7d7
Oleksiy12 <betbonnay@gmail.com>
2023-04-18 21:08:49 +0300

    result

--- jsAll/task7.js
+++ jsAll/task7.js
@@ -2,9 +2,10 @@
 
 const input = document.getElementById("font-size-control");
 const span = document.getElementById("text");
+span.style.fontSize = input.value + "px";
 
-
-input.addEventListener("input", function(){
-  span.style.fontSize = input.value + "px"
+function result(event){
+  span.style.fontSize = event.currentTarget.value + "px"
 }
-)
+
+input.addEventListener("input", result)
